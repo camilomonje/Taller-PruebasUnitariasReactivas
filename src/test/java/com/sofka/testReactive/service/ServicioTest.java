363ba7bf0c8dc04b40866cb8f186f0f829a5e5ce
camilomonje <lumonje12@gmail.com>
2022-04-12 17:56:14 -0500

    Se completa el taller

--- src/test/java/com/sofka/testReactive/service/ServicioTest.java
+++ src/test/java/com/sofka/testReactive/service/ServicioTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
 
 import java.time.Duration;
 
@@ -57,6 +58,49 @@ class ServicioTest {
                 .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException &&
                         throwable.getMessage().equals("Mensaje de Error")
                 ).verify();
+    }
+
+    @Test
+    void testPostEjecucion() {
+        Flux<Integer> source = Flux.<Integer>create(emitter -> {
+            emitter.next(1);
+            emitter.next(2);
+            emitter.next(3);
+            emitter.complete();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            emitter.next(4);
+        }).filter(number -> number % 2 == 0);
+        StepVerifier.create(source)
+                .expectNext(2)
+                .expectComplete()
+                .verifyThenAssertThat()
+                .hasDropped(4)
+                .tookLessThan(Duration.ofMillis(1050));
+    }
+
+    class UppercaseConverter {
+        private final Flux<String> source;
+        UppercaseConverter(Flux<String> source) {
+            this.source = source;
+        }
+        Flux<String> getUpperCase() {
+            return source
+                    .map(String::toUpperCase);
+        }
+    }
+
+    final TestPublisher<String> testPublisher = TestPublisher.create();
+    @Test
+    void testUpperCase() {
+        UppercaseConverter uppercaseConverter = new UppercaseConverter(testPublisher.flux());
+        StepVerifier.create(uppercaseConverter.getUpperCase())
+                .then(() -> testPublisher.emit("datos", "GeNeRaDoS", "Sofka"))
+                .expectNext("DATOS", "GENERADOS", "SOFKA")
+                .verifyComplete();
     }
 
 }
